201f16aaad216d267a47def8a7096edbbb16b248
Jelle Tempelman <j.tempelman@jelletempelman.nl>
2017-03-08 16:54:26 +0100

    no message

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/images.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:600,700|Ubuntu:500,700" rel="stylesheet">
+<style>
+
+</style>
+</head>
+
+<body>
+    <div class="head">
+        <div class="logo headpd">
+            <a href="/"><img class="logo" src="img/logo.png" alt="Home"> </a>
+        </div>
+
+    </div>
+
+</body>
+</html>
